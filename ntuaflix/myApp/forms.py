e261d7244fba6d180c57ced28b5796773fd2986c
Michalis Dimitropoulos <148213577+ntua-el20119@users.noreply.github.com>
2024-02-08 14:56:32 +0200

    Delete ntuaflix directory

--- ntuaflix/myApp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class BasicForm(forms.Form):
-    tsv_file = forms.FileField(label='Upload TSV file')
--- ntuaflix/myApp/templates/my_custom_filters.py
+++
@@ -1,9 +0,0 @@
-# my_custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter(name='replace')
-def replace(value, args):
-    search_str, replace_str = args.split(',')
-    return value.replace(search_str, replace_str)
