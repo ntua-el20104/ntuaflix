e6816d53df817ebebc52016dfac515fb80bbe2e9
ntua-el20643 <el20643@mail.ntua.gr>
2024-02-15 23:38:57 +0200

    fix

--- ntuaflix/settings.py
+++ ntuaflix/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ntuaflix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
